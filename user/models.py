3704365d7c7ad7e8ed0bd618c54dabd107a5192e
一亮 靳 <jinyiliang@shoppingm.cn>
2016-02-05 14:35:00 +0800

    建项目

---
+++ user/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=255)
+    role_id = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    tel = models.CharField(max_length=11)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Role(models.Model):
+    role_id = models.CharField(max_length=20)
+    role_title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.role_title
