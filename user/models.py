b7152d97d8d456680943ac13b9209c0ef6733db3
一亮 靳 <jinyiliang@shoppingm.cn>
2016-02-05 14:45:55 +0800

    添加user view

--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Users(models.Model):
+    user_id = models.CharField(max_length=25)
     name = models.CharField(max_length=255)
     role_id = models.CharField(max_length=20)
     email = models.EmailField()
--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from user.models import Users
+from django.http import Http404
 # Create your views here.
+
+
+def user(request, user_id):
+    try:
+        _post = Users.objects.get(user_id=user_id)
+    except Users.doesNotExist:
+        return Http404
+    
+    return render(request, 'user.html', _post=_post)
+
